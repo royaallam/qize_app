1dd69579edd30f19b5f91dc7abcebaa8e1382d88
roya <royaallam14@gmail.com>
2026-01-29 05:28:14 +0300

    last  commiTTTt

--- lib/data/question.dart
+++ lib/data/question.dart
@@ -1,22 +1,29 @@
 import 'package:flutter_qize_app/modle/quiz_question.dart';
 
 const question = [
-  QuizQuestion('What are the main building blocks of Flutter UIs', [
+  QuizQuestion('What are the main building blocks of Flutter UIs ? ', [
     'Widget',
     'Components',
     'Blacks',
     'Functions',
   ]),
-  QuizQuestion('How are Flutter UIs built', [
+  QuizQuestion('How are Flutter UIs built ?', [
     'by combining widgets in code',
     'by combining widgets in visual editor',
     'by defining widget in config file',
     'by using XCode for IOS and Android  Studio for Androis',
   ]),
-  QuizQuestion('What the purpose of a Stateful widget', [
+  QuizQuestion('What the purpose of a Stateful widget ?' , [
     'Update UI as data changes',
     'Update data as UI changes',
     'Update data changes',
     'Render UI that does not  depend  on data',
   ]),
+  QuizQuestion('Which programming language is used by Flutter ?', [
+    'Dart',
+    'Ruby',
+    'c++',
+    'Kotlin',
+  ]),
+  QuizQuestion('Who created Flutter?', ['Google','Facebook','Adobe','Microsoft'])
 ];
